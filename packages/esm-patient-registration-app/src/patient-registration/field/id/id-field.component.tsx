5e2d02d321b44890b9fda49d078a16531e4fa37c
Jabar Jeremy <24471994+jabahum@users.noreply.github.com>
2024-04-04 12:50:31 +0300

    U4X-321 : Create functionality to capture patient fingerprint (#5)

--- packages/esm-patient-registration-app/src/patient-registration/field/id/id-field.component.tsx
+++ packages/esm-patient-registration-app/src/patient-registration/field/id/id-field.component.tsx
@@ -14,6 +14,7 @@ import {
 } from '../../patient-registration.types';
 import { ResourcesContext } from '../../../offline.resources';
 import styles from '../field.scss';
+import FingerPrintCapture from '../../input/custom-input/fingerprint-capture/fingerprint-capture.component';
 
 export function setIdentifierSource(
   identifierSource: IdentifierSource,
@@ -63,6 +64,7 @@ export const Identifiers: React.FC = () => {
   const { t } = useTranslation();
   const layout = useLayoutType();
   const [showIdentifierOverlay, setShowIdentifierOverlay] = useState(false);
+  const [showFingerPrintOverlay, setShowFingerPrintOverlay] = useState(false);
   const config = useConfig();
   const { defaultPatientIdentifierTypes } = config;
 
@@ -126,10 +128,18 @@ export const Identifiers: React.FC = () => {
           </Button>
         </div>
       </UserHasAccess>
+      <Button
+        kind="ghost"
+        className={styles.setIDNumberButton}
+        onClick={() => setShowFingerPrintOverlay(true)}
+        size={isDesktop(layout) ? 'sm' : 'md'}>
+        {t('addFingerPrint', 'Add FingerPrint')}
+      </Button>
       <div>
         {Object.entries(values.identifiers).map(([fieldName, identifier]) => (
           <IdentifierInput key={fieldName} fieldName={fieldName} patientIdentifier={identifier} />
         ))}
+        {showFingerPrintOverlay && <FingerPrintCapture closeOverlay={() => setShowFingerPrintOverlay(false)} />}
         {showIdentifierOverlay && (
           <IdentifierSelectionOverlay
             setFieldValue={setFieldValue}
